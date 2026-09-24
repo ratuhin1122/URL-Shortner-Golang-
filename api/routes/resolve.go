2fb452abd3913092a38bcced88117c547ac60077
Tuhin <ruhulamintuhin715@gmail.com>
2026-09-24 19:22:05 +0600

    feat: implement URL resolution route, Redis database connection, and initial configuration

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,32 @@
 package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/ratuhin1122/URL-shortner/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	rdb := database.CreateClient(0)
+	defer rdb.Close()
+
+	value, err := rdb.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short URL not found",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "can't connect to server",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter").Result()
+
+	return c.Redirect(value, 301)
+}
